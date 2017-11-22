e783c2900ae52260205b919f80a8d42e329f7f7d
Shinasaki <Shinasaki07@gmail.com>
2017-11-22 23:08:09 +0700

    (Scoure) task detail newline break pre

--- src/components/Course/PracticeTask.vue
+++ src/components/Course/PracticeTask.vue
@@ -23,7 +23,7 @@
                 <div style="word-break: break-all; letter-spacing: 0.5px;">
                   <strong>Description:</strong> 
                 <div style="background: #e6e6e6; border-radius: 3px;">
-                  <p style="text-indent: 10px; padding: 5px; white-space: pre;">{{ taskData.detail }}</p>
+                  <p style="text-indent: 10px; padding: 5px; white-space: pre-wrap;">{{ taskData.detail }}</p>
                 </div>
                 </div>
                 <br>
